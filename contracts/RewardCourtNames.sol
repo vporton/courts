576cc04a51427216489c26fbbeb14eb069ff067a
Victor Porton <porton@narod.ru>
2020-01-24 07:41:41 +0200

    intercourt tokens name events

--- contracts/RewardCourtNames.sol
+++ contracts/RewardCourtNames.sol
@@ -6,10 +6,13 @@ contract RewardCourtNames
 {
     RewardCourts rewardCourts;
 
+    // See https://ethereum.stackexchange.com/a/78986/36438
     // court ID => prevChange
-    mapping (uint256 => uint) public nameChanges;
+    mapping (uint256 => uint) public courtNameChanges;
+    mapping (uint256 => uint) public icTokenNameChanges;
 
     event SetCourtName(uint256 courtId, string name, uint previous);
+    event SetIntercourtTokenName(address owner, uint256 _icToken, string _name, uint previous);
     
     constructor(RewardCourts _rewardCourts) {
         rewardCourts = _rewardCourts;
@@ -17,9 +20,13 @@ contract RewardCourtNames
 
     function setCourtName(uint256 _courtId, string _name) {
         require(rewardCourts.courtOwners(_courtId) == msg.sender, "You don't control this court.");
+        emit SetCourtName(_courtId, _name, courtNameChanges[_courtId]);
+        courtNameChanges[_courtId] = block.number;
+    }
 
-        // See https://ethereum.stackexchange.com/a/78986/36438
-        emit SetCourtName(_courtId, _name, nameChanges[_courtId]);
-        nameChanges[_courtId] = block.number;
+    function setIntercourtTokenName(uint256 _icToken, string _name) {
+        require(owner == msg.sender, "You don't control this court.");
+        emit SetIntercourtTokenName(msg.sender, _icToken, _name, icTokenNameChanges[_courtId]);
+        icTokenNameChanges[_icToken] = block.number;
     }
 }
