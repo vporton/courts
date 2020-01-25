4da6350eddf6349865adddd30fce6e938d09d14e
Victor Porton <porton@narod.ru>
2020-01-25 03:25:08 +0200

    simplified

--- app/src/script.js
+++ app/src/script.js
@@ -7,13 +7,6 @@ export const app = new Aragon()
 app.store(async (state, { event }) => {
   let nextState = { ...state }
 
-  // Initial state
-  if (nextState.ownedContract == null) {
-    nextState.ownedContract = await getOwnedContract()
-    nextState.courtNamesContract = await getCourtNamesContract()
-    nextState.courtId = await getCourtId()
-  }
-
   switch (event) {
     case events.SYNC_STATUS_SYNCING:
       nextState = { ...nextState, isSyncing: true }
@@ -25,21 +18,3 @@ app.store(async (state, { event }) => {
 
   return nextState
 })
-
-// app.state().subscribe(
-//   state => {
-//   }
-// )
-// 
-
-async function getOwnedContract() {
-  return app.call('ownedContract').toPromise()
-}
-
-async function getCourtNamesContract() {
-  return app.call('courtNamesContract').toPromise()
-}
-
-async function getCourtId() {
-  return app.call('courtId').toPromise()
-}
